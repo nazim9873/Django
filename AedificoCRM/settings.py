ad4be733c2c081000d543ac70d52d55e28acee47
nazim9873 <nazim9873@gmail.com>
2021-12-10 23:10:17 +0530

    wewr

--- AedificoCRM/settings.py
+++ AedificoCRM/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_truncate',
     'crm.apps.CrmConfig',
     'members.apps.MembersConfig'
 ]
