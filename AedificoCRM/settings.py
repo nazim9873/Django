9756d35887f49cd0b9273019b884fd85ac51024c
nazim9873 <nazim9873@gmail.com>
2021-12-10 18:39:19 +0530

    stat

--- AedificoCRM/settings.py
+++ AedificoCRM/settings.py
@@ -117,12 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,4 +130,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
-django_heroku.settings(locals())
