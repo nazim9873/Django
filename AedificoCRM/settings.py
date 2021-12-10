060c41fc112747d12086ed35c839431a38814f35
nazim9873 <nazim9873@gmail.com>
2021-12-10 17:03:47 +0530

    wewqeqw

--- AedificoCRM/settings.py
+++ AedificoCRM/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-)t*yc1^!76f_#ht)ka(e4_9b6vbgs9k5z@yotcfjfn!b1$g(n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["crm-aedifico.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["crm-aedificotech.herokuapp.com", "localhost"]
 
 # Application definition
 
