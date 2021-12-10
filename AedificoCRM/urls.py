cf0d5fcba8737eea6cd01101360b94f2e4665c97
nazim9873 <nazim9873@gmail.com>
2021-12-11 01:04:06 +0530

    last

--- AedificoCRM/urls.py
+++ AedificoCRM/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
                   path("", include('crm.urls')),
                   path("members/", include('members.urls')),
                   path("members/", include('django.contrib.auth.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
