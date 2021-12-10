56b6a6eed0bed3059bac4575748eabf733673b91
nazim9873 <nazim9873@gmail.com>
2021-12-10 23:56:05 +0530

    #4

--- AedificoCRM/settings.py
+++ AedificoCRM/settings.py
@@ -125,6 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
